7cf14c5e0d74bd3e08f3898611387c92e9aecdfd
jtrotsky <5625795+jtrotsky@users.noreply.github.com>
2018-05-28 12:25:26 +1200

    more cmd flags

--- main.go
+++ main.go
@@ -10,30 +10,35 @@ import (
 )
 
 const (
-	defaultAssetName = "Chroma 3 Case"
+	defaultAssetName string = "AK-47 | Case Hardened"
+	defaultWearTier  int    = 3
 )
 
 var (
 	assetName   string
 	steamAPIKey string
+	wearTier    int
+	statTrak    bool
 )
 
 func init() {
-	flag.StringVar(&assetName, "name", defaultAssetName, "the name of the Steam asset to lookup")
-	flag.StringVar(&steamAPIKey, "key", "", "the user Steam Web API Key")
+	flag.StringVar(&assetName, "n", defaultAssetName, "the name of the Steam asset to query")
+	flag.StringVar(&steamAPIKey, "k", "", "the user Steam Web API Key")
+	flag.IntVar(&wearTier, "w", defaultWearTier, "what wear quality to query")
+	flag.BoolVar(&statTrak, "s", false, "whether to query items with StatTrak")
 	flag.Parse()
 }
 
 func main() {
 	if steamAPIKey == "" {
-		log.Fatal("please specify API Key")
+		log.Fatal("please specify an API Key")
 	}
 
-	asset := steam.NewAsset(assetName)
-
-	if asset.EncodedName == "" {
-		log.Fatalf("encoded name blank: %s", asset.EncodedName)
+	if wearTier > 5 || wearTier < 1 {
+		log.Fatal("please specify a wear tear between 1 and 5")
 	}
+
+	asset := steam.NewAsset(assetName, wearTier, statTrak)
 
 	marketListing, err := steam.GetMarketListing(asset.EncodedName)
 	if err != nil {
