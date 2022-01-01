e6fc88b1345b56044b5fdbdbfd1af65d480294b9
jtrotsky <5625795+jtrotsky@users.noreply.github.com>
2022-01-01 17:09:32 +0000

    start migrating apis

--- steam/market.go
+++ steam/market.go
@@ -9,125 +9,104 @@ import (
 	"strconv"
 )
 
-const (
-	marketBaseURL       string = "https://steamcommunity.com"
-	marketLanguage      string = "en_US"
-	marketCountry       string = "us"
-	marketCurrency      string = "1" // USD (https://github.com/SteamRE/SteamKit/blob/master/Resources/SteamLanguage/enums.steamd#L855).
-	marketDataFormat    string = "json"
-	marketStartingIndex int    = 0
+//https://steamcommunity.com/market/search/render/
+//?sort_column=default
+//&sort_dir=desc
+//&appid=730
+//&norender=1
+//&count=25
+//&query=AK-47%20%7C%20Case%20Hardened%20%28Field-Tested%29
 
-	pathPriceOverview string = "market/priceoverview"
-	pathMarketListing string = "market/listings"
+const (
+	marketBaseURL    string = "https://steamcommunity.com"
+	marketSearchPath string = "/market/search/render/"
+
+	// pathPriceOverview string = "market/priceoverview"
+	// pathMarketListing string = "market/listings"
 )
 
-// MarketListing is an item listed on the Steam market.
-type MarketListing struct {
-	PageSize   string `json:"pagesize,omitempty"`
-	TotalCount int    `json:"total_count,omitempty"`
-	Start      int    `json:"start,omitempty"`
+// MarketSearch is a search for an item on the Steam market.
+type MarketSearch struct {
+	Success    bool `json:"success,omitempty"`
+	PageSize   int  `json:"pagesize,omitempty"`
+	TotalCount int  `json:"total_count,omitempty"`
+	Start      int  `json:"start,omitempty"`
 
-	ListingInfo map[string]Listing                     `json:"listinginfo,omitempty"`
-	Assets      map[string]map[string]map[string]Asset `json:"assets,omitempty"`
+	SearchData SearchData `json:"searchdata,omitempty"`
+	Results    []Listing
 }
 
-// Listing contains information specific to the market listing such as its price.
+// SearchData summarises the requested market search and the results found.
+type SearchData struct {
+	Query              string `json:"query,omitempty"`
+	SearchDescriptions bool   `json:"search_descriptions,omitempty"`
+	TotalCount         int    `json:"total_count,omitempty"`
+	PageSize           int    `json:"pagesize,omitempty"`
+	Prefix             string `json:"prefix,omitempty"`
+	ClassPrefix        string `json:"class_prefix,omitempty"`
+}
+
+// Listing represents an item for sale on the market including its quality and price.
 type Listing struct {
-	ID    string      `json:"listingid,omitempty"`
-	Price int         `json:"converted_price,omitempty"`
-	Fee   int         `json:"converted_fee,omitempty"`
-	Asset MarketAsset `json:"asset,omitempty"`
+	Name             string           `json:"name,omitempty"`
+	HashName         string           `json:"hash_name,omitempty"`
+	SellListings     int              `json:"sell_listings,omitempty"`
+	SellPrice        int              `json:"sell_price,omitempty"`
+	SellPriceText    string           `json:"sell_price_text,omitempty"`
+	AppIcon          string           `json:"app_icon,omitempty"`
+	AppName          string           `json:"app_name,omitempty"`
+	AssetDescription AssetDescription `json:"asset_description,omitempty"`
+	SalePriceText    string           `json:"sale_price_text,omitempty"`
 }
 
-// MarketAsset is a summary of an asset listed on the market.
-type MarketAsset struct {
-	ID            string         `json:"id,omitempty"`
-	Currency      int            `json:"currency,omitempty"`
-	AppID         int            `json:"appid,omitempty"`
-	ContextID     string         `json:"contextid,omitempty"`
-	Amount        string         `json:"amount,omitempty"`
-	MarketActions []MarketAction `json:"market_actions,omitempty"`
+// AssetDescription contains details about an item.
+type AssetDescription struct {
+	AppID            int    `json:"appid,omitempty"`
+	ClassID          string `json:"classid,omitempty"`
+	InstanceID       string `json:"instanceid,omitempty"`
+	BackgroundColour string `json:"appid,omitempty"`
+	IconURL          string `json:"background_color,omitempty"`
+	Tradable         bool   `json:"tradable,omitempty"`
+	Name             string `json:"name,omitempty"`
+	NameColour       string `json:"name_color,omitempty"`
+	Type             string `json:"type,omitempty"`
+	MarketName       string `json:"market_name,omitempty"`
+	MarketHashName   string `json:"market_hash_name,omitempty"`
+	Commodity        bool   `json:"commodity,omitempty"`
 }
 
-// MarketAction is an action that can be taken such as inspect the item in game.
-type MarketAction struct {
-	Link string `json:"link,omitempty"`
-	Name string `json:"name,omitempty"`
-}
-
-// GetMarketListing returns info about an asset listed on the Steam market.
-func (client *Client) GetMarketListing(encodedName string, listings int, debug bool) (*MarketListing, error) {
-	marketListingURL, err := url.Parse(fmt.Sprintf("%s/%s/%s/%s/render", marketBaseURL, pathMarketListing, csgoAppID, encodedName))
+// Search returns listings for an asset on the Steam market.
+func (client *Client) Search(encodedItemName string, searchCount int, debug bool) (*MarketSearch, error) {
+	marketSearchURL, err := url.Parse(fmt.Sprintf("%s%s", marketBaseURL, marketSearchPath))
 	if err != nil {
 		return nil, err
 	}
 
 	params := url.Values{}
-	params.Add("start", strconv.Itoa(marketStartingIndex))
-	params.Add("count", strconv.Itoa(listings))
-	params.Add("currency", marketCurrency)
-	params.Add("language", marketLanguage)
-	params.Add("format", marketDataFormat)
-	params.Add("appid", csgoAppID)
-	marketListingURL.RawQuery = params.Encode()
+	params.Add("appid", strconv.Itoa(CSGOApplicationID))
+	// params.Add("sort_column", "default")
+	// params.Add("sort_dir", "desc")
+	params.Add("norender", strconv.Itoa(1)) // get results as JSON not HTML
+	params.Add("count", strconv.Itoa(searchCount))
+	params.Add("query", encodedItemName)
+	marketSearchURL.RawQuery = params.Encode()
 
 	// DEBUG
 	if debug {
-		log.Println(marketListingURL)
+		log.Println(marketSearchURL)
 	}
 
-	response, err := http.DefaultClient.Get(marketListingURL.String())
+	response, err := http.DefaultClient.Get(marketSearchURL.String())
 	if err != nil {
 		return nil, err
 	}
 	defer response.Body.Close()
 
-	marketListing := MarketListing{}
-	err = json.NewDecoder(response.Body).Decode(&marketListing)
+	marketSearch := MarketSearch{}
+	err = json.NewDecoder(response.Body).Decode(&marketSearch)
 	if err != nil {
 		return nil, err
 	}
 
-	return &marketListing, err
+	return &marketSearch, err
 }
-
-// // GetPriceSummary returns basic market price statistics for a given asset.
-// func (asset *SimpleAsset) GetPriceSummary(debug bool) error {
-// 	assetPriceURL, err := url.Parse(fmt.Sprintf("%s/%s", marketBaseURL, pathPriceOverview))
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	params := url.Values{}
-// 	params.Add("format", marketDataFormat)
-// 	params.Add("country", marketCountry)
-// 	params.Add("currency", marketCurrency)
-// 	params.Add("appid", csgoAppID)
-// 	assetPriceURL.RawQuery = params.Encode()
-
-// 	assetPriceURL.RawQuery += fmt.Sprintf("&market_hash_name=%s", asset.EncodedName)
-
-// 	// DEBUG
-// 	if debug {
-// 		log.Println(assetPriceURL)
-// 	}
-
-// 	response, err := http.DefaultClient.Get(assetPriceURL.String())
-// 	if err != nil {
-// 		return err
-// 	}
-// 	defer response.Body.Close()
-
-// 	err = json.NewDecoder(response.Body).Decode(&asset.MarketValue)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	if marketCurrency == "1" {
-// 		asset.MarketValue.Currency = "USD"
-// 	} else {
-// 		asset.MarketValue.Currency = "?"
-// 	}
-
-// 	return err
-// }
--- steam/steam.go
+++ steam/steam.go
@@ -1,7 +1,7 @@
 package steam
 
 const (
-	csgoAppID               string = "730" // Steam Application ID for CSGO.
+	CSGOApplicationID       int    = 730 // Steam Application ID for CSGO.
 	steamAPIBaseURL         string = "https://api.steampowered.com"
 	steamImageCDNBaseURL    string = "https://steamcommunity-a.akamaihd.net/economy/image/"
 	steamImageCDNBaseURLOld string = "https://cdn.steamcommunity.com/economy/image/"
@@ -9,18 +9,18 @@ const (
 
 // Client is the Steam client that contains config and authentication.
 type Client struct {
-	APIKey     string
-	CSGOAppID  string
-	CDNBaseURL string
-	APIBaseURL string
+	APIKey            string
+	CSGOApplicationID int
+	CDNBaseURL        string
+	APIBaseURL        string
 }
 
 // NewClient initiates a Steam client.
 func NewClient(apiKey string) *Client {
 	return &Client{
-		APIKey:     apiKey,
-		CSGOAppID:  csgoAppID,
-		CDNBaseURL: steamImageCDNBaseURL,
-		APIBaseURL: steamAPIBaseURL,
+		APIKey:            apiKey,
+		CSGOApplicationID: CSGOApplicationID,
+		CDNBaseURL:        steamImageCDNBaseURL,
+		APIBaseURL:        steamAPIBaseURL,
 	}
 }
